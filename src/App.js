f019ea6b0aac1f3c4f276cb923a5b0bf1c127d83
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-04-27 14:15:38 -0400

    added footer with link to GitHub

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Your Weather App
+     <div className="container">
+        <div className="WeatherApp">
+          
+
+          <footer>
+            This project coded by Jennifer Griffin and is 
+            <a href="https://github.com/coderGriff66/first-weather-app-react" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
